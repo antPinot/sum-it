d67162d15048299d12b25729aead767eaa667b5e
unknown <pinot.anthony@gmail.com>
2023-11-16 13:50:11 +0100

    Implémentation géolocalisation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilsService } from '../services/utils/utils.service';
 import { Router } from '@angular/router';
-import { Map, latLng, tileLayer } from 'leaflet';
+import { Icon, Map, icon, latLng, marker, tileLayer } from 'leaflet';
+import * as L from 'leaflet';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -24,13 +26,23 @@ export class HomePage implements OnInit {
 
   layersControl = {
     baseLayers: {
-      'Open Street Map': tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      'Open Street Map': tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      'Open Topo Map': tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
     },
     overlays: {
     }
   }
 
-  constructor(private utilsService : UtilsService, private router:Router) {}
+  pin = marker([0, 0], {
+    icon: icon({
+      ...Icon.Default.prototype.options,
+      iconUrl: '../../assets/icon/marker-icon.png',
+      iconRetinaUrl: '../../assets/icon/marker-icon-2x.png',
+      shadowUrl: '../../assets/icon/marker-shadow.png'
+    })
+  })
+
+  constructor(private utilsService: UtilsService, private router: Router) { }
 
   ngOnInit(): void {
     this.title = this.utilsService.getTitleFromUrl(this.router.url)
@@ -38,7 +50,28 @@ export class HomePage implements OnInit {
 
   onMapReady(map: Map) {
     this.map = map;
-    setTimeout(() => this.map.invalidateSize(true), 100)
+    setTimeout(() => this.map.invalidateSize(true), 500)
+    let locateButton = L.Control.extend({
+      options: {
+        position: 'topleft'
+      },
+
+      onAdd: (map: Map) => {
+        var container = L.DomUtil.create('img', 'leaflet-bar leaflet-control leaflet-control-custom');
+        container.style.backgroundColor = 'white';
+        container.style.width = '30px';
+        container.style.height = '30px';
+        container.src = "../../assets/icon/locate.png"
+
+        container.onclick = () => Geolocation.getCurrentPosition().then((position) => {
+          console.log('coucou')
+          this.pin.setLatLng([position.coords.latitude, position.coords.longitude]).addTo(this.map)
+        });
+        return container;
+      }
+    })
+
+    this.map.addControl(new locateButton())
   }
 
 }
